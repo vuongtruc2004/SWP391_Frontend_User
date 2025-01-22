0d4ad0af1bc63b75b4a5c8b3d27e4d62c5ff8527
NguyenTruc2k4 <vuongtruc2004@gmail.com>
2025-01-22 21:39:42 +0700

    add step

--- components/register-step/register-all-step/first.step.tsx
+++ components/register-step/register-all-step/first.step.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const FirstStep = () => {
     return (
         <div>FirstStep</div>
--- components/register-step/register-all-step/second.step.tsx
+++ components/register-step/register-all-step/second.step.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const SecondStep = () => {
     return (
         <div>SecondStep</div>
