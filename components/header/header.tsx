7bad5b31667e480e74de16e5c672159d2782d133
NguyenTruc2k4 <vuongtruc2004@gmail.com>
2025-02-11 09:20:21 +0700

    test push git mail truong

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -19,6 +19,7 @@ import { motion } from 'framer-motion';
 import Image from "next/image";
 
 const Header = () => {
+    /// ok
     const pathname = usePathname();
     const router = useRouter();
     const searchParams = useSearchParams();
